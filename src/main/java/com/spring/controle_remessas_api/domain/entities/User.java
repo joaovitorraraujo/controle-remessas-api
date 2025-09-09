47c76a9c87ee5984bf78473fb1c4c79b9fb8df14
Joao Vitor Correia <joaovitor19.03.2004@gmail.com>
2025-09-09 16:39:03 -0300

    feat: cria estrutura inicial para User, UserRepository, UserDTO

---
+++ src/main/java/com/spring/controle_remessas_api/domain/entities/User.java
@@ -0,0 +1,18 @@
+package com.spring.controle_remessas_api.domain.entities;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false, unique = true, updatable = false)
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+}
---
+++ src/main/java/com/spring/controle_remessas_api/domain/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.spring.controle_remessas_api.domain.repositories;
+
+import com.spring.controle_remessas_api.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
